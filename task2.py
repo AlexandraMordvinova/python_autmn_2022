30f9a937ceb25e682e1bf7ac7d7da9bc2817bf84
AlexandraMordvinova <113683833+AlexandraMordvinova@users.noreply.github.com>
2022-09-19 00:04:47 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,27 @@
+#Преобразуйте переменную age и foo в число 
+age = "23"
+foo = "23abc"
+#Преобразуйте переменную age в Boolean
+age = 123abc
+#Преобразуйте переменную flag в Boolean
+flag = 1
+#Преобразуйте значение  в Boolean
+str_one = "Privet"
+str_two = ""
+#Преобразуйте значение 0 и 1  в Boolean
+
+#1
+age=input("23")
+print(int(age))
+foo=input(123abc)
+print(int(foo))
+#2
+bool(123abc)
+#3
+bool(1)
+#4
+bool("Privet")
+bool("")
+#5
+bool(0)
+bool(1)
---
+++ task4.py
@@ -0,0 +1,9 @@
+# todo:  Создайте переменные, что бы вывести требуемый текст.
+
+# задан нерабочий код
+# требуемый вывод:
+# Мой стек: python, javascript, php
+
+print("Мой стек:", programming_lang_1, programming_lang_1 lang_2, programming_lang_3)
+
+print("Мой стек:" "python," "javascript," "php")
