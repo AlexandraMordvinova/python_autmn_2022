e68cd2c50a2a5b9f9af55d2195b14e627556cefa
Alexandra <sole1983@mail.ru>
2022-09-19 20:52:37 +0300

    dz 1

--- task4.py
+++
@@ -1,9 +0,0 @@
-# todo:  Создайте переменные, что бы вывести требуемый текст.
-
-# задан нерабочий код
-# требуемый вывод:
-# Мой стек: python, javascript, php
-
-print("Мой стек:", programming_lang_1, programming_lang_1 lang_2, programming_lang_3)
-
-print("Мой стек:" "python," "javascript," "php")
