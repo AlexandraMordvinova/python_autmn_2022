1b99a030bd8099e7aa060507669dbe8df548bbfb
Alexandra <sole1983@mail.ru>
2022-10-03 16:41:07 +0300

    dz

--- unit_one/task17.py
+++ unit_one/task17.py
@@ -1,9 +1,26 @@
 #todo: Создайте функцию compute_bill, считающаю итоговую сумму товаров в чеке.
-Функция должна принимать 1 параметр - словарь, в котором указано количество едениц товара.
-Цены хранятся в словаре:
+#Функция должна принимать 1 параметр - словарь, в котором указано количество едениц товара.
+#Цены хранятся в словаре:
 prices = {
   "banana": 4,
   "apple": 2,
   "orange": 1.5,
   "pear": 3
 }
+shopping_list = ['banana', 'apple', 'orange', 'pear']
+import csv
+with open('prices.csv', 'r+', encoding='utf-8') as p:
+  reader = csv.reader(p, delimiter=',')
+  count = 0
+  for row in reader:
+    prices[row[0]] = row[1]
+input_file = csv.DictReader(open('prices.csv'))
+
+def compute_bill(prices):
+  total = 0
+  for a in food:
+    total += prices[a]
+  return total
+
+
+
